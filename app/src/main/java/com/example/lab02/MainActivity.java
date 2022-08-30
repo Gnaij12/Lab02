503832be35d05e1c13a5d5057758182b67517920
Jiang <1532269@fcps.edu>
2022-08-30 11:56:57 -0400

    Initial commit

---
+++ app/src/main/java/com/example/lab02/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.lab02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button incrementButton;
+    TextView greetingDisplay;
+    int count = 0;
+    int multiplier = 1;
+    String[] planets;
+    RadioButton leftRadBut, rightRadBut;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        incrementButton = findViewById(R.id.increment_button);
+        greetingDisplay = findViewById(R.id.greeting_textview);
+        planets = getResources().getStringArray(R.array.planets_array);
+        leftRadBut = findViewById(R.id.radio_left);
+        rightRadBut = findViewById(R.id.radio_right);
+        incrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                System.out.println("incrementing: " + (count+=multiplier));
+//                Log.i("incrementing",""+count);
+//                greetingDisplay.setText(getString(R.string.likes_count,count));
+                if (rightRadBut.isChecked())
+                    count = ++count%planets.length;
+                greetingDisplay.setText(planets[count]);
+            }
+        });
+    }
+
+    public void decrement(View view) {
+//        System.out.println("decrementing: " + (count-=multiplier));
+//        greetingDisplay.setText(getString(R.string.likes_count,count));
+        if (leftRadBut.isChecked())
+            if (--count <0) count=planets.length-1;
+        greetingDisplay.setText(planets[count]);
+    }
+    public void random(View view) {
+        count = (int) (Math.random()*20-10)*multiplier;
+        System.out.println("randomizing: " + count);
+        greetingDisplay.setText(getString(R.string.likes_count,count));
+    }
+    public void ten(View view) {
+        multiplier = 10;
+        System.out.println("Multiplier is now " + multiplier);
+    }
+    public void one(View view) {
+        multiplier = 1;
+        System.out.println("Multiplier is now " + multiplier);
+    }
+
+
+
+}
