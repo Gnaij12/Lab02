0a4e65ed58d70035f6f50fb226d23856b17f40c6
1532269 <alanzjiang10@gmail.com>
2022-09-06 10:39:43 -0400

    Kirbo version

--- app/src/main/java/com/example/lab02/MainActivity.java
+++ app/src/main/java/com/example/lab02/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.lab02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     int multiplier = 1;
     String[] planets;
     RadioButton leftRadBut, rightRadBut;
+    int[] back_images;
+    ImageView background;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         rightRadBut = findViewById(R.id.radio_right);
         displayText = findViewById(R.id.displayInputText);
         inputText = findViewById(R.id.inputText);
+        back_images = new int[]{R.drawable.kirby1, R.drawable.kirby2,
+                R.drawable.kirby3, R.drawable.kirby4};
+        background = findViewById(R.id.background);
+        background.setScaleType(ImageView.ScaleType.FIT_XY);
         incrementButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,8 +44,8 @@ public class MainActivity extends AppCompatActivity {
 //                Log.i("incrementing",""+count);
 //                greetingDisplay.setText(getString(R.string.likes_count,count));
                 if (rightRadBut.isChecked())
-                    count = ++count%planets.length;
-                greetingDisplay.setText(planets[count]);
+                    count = ++count%back_images.length;
+                background.setImageResource(back_images[count]);
             }
         });
     }
@@ -46,8 +54,8 @@ public class MainActivity extends AppCompatActivity {
 //        System.out.println("decrementing: " + (count-=multiplier));
 //        greetingDisplay.setText(getString(R.string.likes_count,count));
         if (leftRadBut.isChecked())
-            if (--count <0) count=planets.length-1;
-        greetingDisplay.setText(planets[count]);
+            if (--count <0) count=back_images.length-1;
+        background.setImageResource(back_images[count]);
     }
     public void random(View view) {
         count = (int) (Math.random()*20-10)*multiplier;
